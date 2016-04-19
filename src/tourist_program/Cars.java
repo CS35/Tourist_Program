28505ebb33607c92d5fd37a389548abadb4c2b77
hindfh <Apple@Apples-MacBook-Pro.local>
2016-04-19 21:51:44 +0300

    xx

--- src/tourist_program/Cars.java
+++ src/tourist_program/Cars.java
@@ -6,4 +6,15 @@ package tourist_program;
 public class Cars 
 {
     
+    String[][] cars = {{"car name","GMC Yukon","Toyota Yaris","Hyundai"}
+           ,{"size","7","5","5"}
+           ,{"price","700","306","326.5"}};
+    
+//print 2D cars array
+for(int row=0 ; row < cars.length ; row++){
+        for (String car : cars[row]) {
+            System.out.print(car);
+        }
+System.out.println();
+}
 }
--- src/tourist_program/TouristArea.java
+++ src/tourist_program/TouristArea.java
@@ -3,5 +3,19 @@ package tourist_program;
 
 public interface TouristArea 
 {
-    
+    //France
+    public void printParisTurestArea();
+    public void printNiceTurestArea();
+    public void printLyonTurestArea();
+    public void printMarseilleTurestArea();
+    //United Kongdom
+    public void printLondonTurestArea();
+    public void printEdinburghTurestArea();
+    public void printBirminghamTurestArea();
+    public void printLiverpoolTurestArea();
+    //Turkey
+    public void printIstanbulTurestArea();
+    public void printAntalyaTurestArea();
+    public void printUzungolTurestArea();
+    public void printTrabzonTurestArea();
 }
