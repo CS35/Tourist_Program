9be621e709a99b3b0cd5c1f17e9bfd85b5dca466
Joanna <Joanna.a.s.1996@Gmail.com>
2016-04-27 21:20:59 +0300

    last one was a mistakegit add .

--- src/tourist_program/SubArea.java
+++ src/tourist_program/SubArea.java
@@ -79,50 +79,50 @@ public class SubArea implements TouristArea
     public String printNiceTouristArea(){
        return "Tourist Area in Nice:\n"
                + " -Musee Marc Chagall\n -Cours Saleya\n -Paris panorama \n"
-               + "\"Here's a guide number if you need one: +33 6 84 87 85 45";
+               + "Here's a guide number if you need one: +33 6 84 87 85 45";
     }
     @Override
     public String printLyonTouristArea(){
         return "Tourist Area in Lyon:\n"
                + " -Lyon Cathedral\n -Mur des Canuts\n -Rue St-jean \n"
-                + "\"Here's a guide number if you need one: +33 06 14 42 84 41";
+                + "Here's a guide number if you need one: +33 06 14 42 84 41";
     }
     //United Kongdom
     @Override
     public String printLondonTouristArea(){
         return "Tourist Area in London:\n"
                + " -Palace of Westminster\n -London Eye\n -British Museum \n"
-                + "\"Here's a guide number if you need one: +44 7941 809534";
+                + "Here's a guide number if you need one: +44 7941 809534";
     }
     @Override
     public String printEdinburghTouristArea(){
         return "Tourist Area in Edinburgh:\n"
                + " -Loch Lomond\n -Royal Mile\n -Grassmarket \n"
-                + "\"Here's a guide number if you need one: +44 7899 878857";
+                + "Here's a guide number if you need one: +44 7899 878857";
     }
     @Override
     public String printLiverpoolTouristArea(){
         return "Tourist Area in Liverpool:\n"
                + " -Museum of Liverpool\n -Casbah Coffee  Club\n -Walker Art Gallery \n"
-                + "\"Here's a guide number if you need one: 07918 655113";
+                + "Here's a guide number if you need one: 07918 655113";
     }
     //Turkey
     @Override
     public String printIstanbulTouristArea(){
         return "Tourist Area in Istanbul:\n"
                + " -Grand Bazzar\n -Aya sofya\n -Blue Mosque \n"
-                + "\"Here's a guide number if you need one: +90 212 212 0621";
+                + "Here's a guide number if you need one: +90 212 212 0621";
     }
     @Override
     public String printUzungolTouristArea(){
         return "Tourist Area in Uzungol:\n"
                + " -Walk round the lake\n -Black lack\n -Fish farms \n"
-                + "\"Here's a guide number if you need one: 0462 656 60 21";
+                + "Here's a guide number if you need one: 0462 656 60 21";
     }
     @Override
     public String printTrabzonTouristArea(){
         return "Tourist Area in Trabzon:\n"
                + " -Avrupali Pazari\n -Kaymakli Monastery\n -Boztepe \n"
-                + "\"Here's a guide number if you need one:  05386450537";
+                + "Here's a guide number if you need one:  05386450537";
     }
     }
